908f611f6f673f10bdaf87915016eb3e23efdf13
Xinye Pan <z5577593@ad.unsw.edu.au>
2025-11-12 12:24:02 +1100

    Add AirBrB app router into App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
-
+import { AuthProvider } from './context/AuthContext';
+import AppRouter from './routes/AppRouter';
 function App() {
   const [count, setCount] = useState(0);
   
@@ -27,6 +28,11 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <div style={{ marginTop: '2rem', borderTop: '1px solid #eee', paddingTop: '1rem' }}>
+        <AuthProvider>
+          <AppRouter />
+        </AuthProvider>
+      </div>
     </>
   )
 }
