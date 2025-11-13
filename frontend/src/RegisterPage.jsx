aff13f4abfcea8e0b08d7deeac0bec0ce71253ad
Xinye Pan <z5577593@ad.unsw.edu.au>
2025-11-13 22:15:22 +1100

    import function for register page

---
+++ frontend/src/RegisterPage.jsx
@@ -0,0 +1,7 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useAuthContext } from '../context/AuthContext';
+import {
+  ErrorNotification,
+  SuccessNotification,
+} from '../components/Common/Notification';
