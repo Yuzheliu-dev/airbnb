bf5c2f1f4b1e4142ec665142820b6767149d420a
Xinye Pan <z5577593@ad.unsw.edu.au>
2025-11-13 22:31:53 +1100

    add AllListingsPag

---
+++ frontend/src/pages/AllListingsPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function AllListingsPage() {
+  return (
+    <div>
+      <h1 style={{ fontSize: '1.5rem', marginBottom: '1rem' }}>
+        All Listings
+      </h1>
+      <p>
+        This is your landing page (Feature Set 2.3). Published listings will be
+        shown here later.
+      </p>
+    </div>
+  );
+}
