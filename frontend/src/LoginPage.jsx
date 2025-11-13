131cb8ae7c14c5483fea90472c4367df258e6906
Xinye Pan <z5577593@ad.unsw.edu.au>
2025-11-13 22:12:48 +1100

    export default function for LoginPage

--- frontend/src/LoginPage.jsx
+++ frontend/src/LoginPage.jsx
@@ -5,3 +5,20 @@ import {
   ErrorNotification,
   SuccessNotification,
 } from '../components/Common/Notification';
+export default function LoginPage() {
+    const { login } = useAuthContext();
+    const navigate = useNavigate();
+    const location = useLocation();
+  
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+  
+    const [errorMsg, setErrorMsg] = useState('');
+    const [successMsg, setSuccessMsg] = useState('');
+  
+    const fromPath = location.state?.from?.pathname || '/';
+  
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+      setErrorMsg('');
+      setSuccessMsg('');
