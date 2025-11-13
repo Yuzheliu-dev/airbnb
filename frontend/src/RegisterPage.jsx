c568891ef806a219e1cc359c36f8dd444d3e5d96
Xinye Pan <z5577593@ad.unsw.edu.au>
2025-11-13 22:18:14 +1100

    setting label srtyle

--- frontend/src/RegisterPage.jsx
+++ frontend/src/RegisterPage.jsx
@@ -46,4 +46,66 @@ export default function RegisterPage() {
             </p>
           </div>
   
+          <ErrorNotification
+          message={errorMsg}
+          onClose={() => setErrorMsg('')}
+        />
+        <SuccessNotification
+          message={successMsg}
+          onClose={() => setSuccessMsg('')}
+        />
+
+        <form onSubmit={handleSubmit} style={formStyle}>
+          <label style={labelStyle}>
+            <span style={labelTextStyle}>Name</span>
+            <input
+              type="text"
+              required
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              style={inputStyle}
+              autoComplete="name"
+              placeholder="your name"
+            />
+          </label>
+
+          <label style={labelStyle}>
+            <span style={labelTextStyle}>Email</span>
+            <input
+              type="email"
+              required
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              style={inputStyle}
+              autoComplete="email"
+              placeholder="you@example.com"
+            />
+          </label>
+
+          <label style={labelStyle}>
+            <span style={labelTextStyle}>Password</span>
+            <input
+              type="password"
+              required
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              style={inputStyle}
+              autoComplete="new-password"
+              placeholder="••••••••"
+            />
+          </label>
+
+          <label style={labelStyle}>
+            <span style={labelTextStyle}>Confirm Password</span>
+            <input
+              type="password"
+              required
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              style={inputStyle}
+              autoComplete="new-password"
+              placeholder="••••••••"
+            />
+          </label>
+
          
