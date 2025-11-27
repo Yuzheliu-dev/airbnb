1b2bd9818f7fb3fdfd04fae3e445b19efe57ad22
Xinye Pan <z5577593@ad.unsw.edu.au>
2025-11-27 23:35:55 +0800

    feat: expose notifications context at app root

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,13 @@
 import { AuthProvider } from './context/AuthContext';
+import { NotificationsProvider } from './context/NotificationsContext';
 import AppRouter from './routes/AppRouter';
 
 export default function App() {
   return (
     <AuthProvider>
-      <AppRouter />
+      <NotificationsProvider>
+        <AppRouter />
+      </NotificationsProvider>
     </AuthProvider>
   );
 }
